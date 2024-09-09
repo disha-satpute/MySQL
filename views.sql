505cbcc0081fbeb005f86209b406dafcfddb2715
Disha Satpute <dishasatpute44@gmail.com>
2024-09-09 19:59:30 +0530

    views.sql:update view

--- views.sql
+++ views.sql
@@ -3,4 +3,14 @@ USE collage;
 CREATE VIEW Clg_Teacher AS
 SELECT Teacher_id,first_name,last_name
 FROM Teacher
-WHERE DeptName = 'IT';
+WHERE DeptID= 3;
+
+select * from Clg_Teacher;
+
+  SET SQL_SAFE_UPDATES = 0;
+  
+UPDATE Clg_Teacher
+SET first_name = "madhuri"
+WHERE Teacher_id = 25;
+
+select * from Clg_Teacher;
