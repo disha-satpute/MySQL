40e63137fce2d6ff0398255c22d07128c460febc
Disha Satpute <dishasatpute44@gmail.com>
2024-09-09 20:02:09 +0530

    views.sql:drop view

--- views.sql
+++ views.sql
@@ -1,5 +1,6 @@
 USE collage;
 
+  /* Create View */
 CREATE VIEW Clg_Teacher AS
 SELECT Teacher_id,first_name,last_name
 FROM Teacher
@@ -9,8 +10,17 @@ select * from Clg_Teacher;
 
   SET SQL_SAFE_UPDATES = 0;
   
+  
+  /* Update View */
+  
 UPDATE Clg_Teacher
 SET first_name = "madhuri"
 WHERE Teacher_id = 25;
+
+select * from Clg_Teacher;
+
+  /* Drop View */
+
+Drop view Clg_Teacher;
 
 select * from Clg_Teacher;
