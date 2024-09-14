74f14494b503f964178b08407dea4206ddecca92
Disha Satpute <dishasatpute44@gmail.com>
2024-09-14 23:13:41 +0530

    table created

---
+++ procedures.sql
@@ -0,0 +1,11 @@
+
+create database collage;
+
+use collage;
+
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    DateOfBirth DATE
+);
