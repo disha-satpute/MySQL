3bee71365641da4ead5614c9d91f5c40ffb8dea3
Disha Satpute <dishasatpute44@gmail.com>
2024-09-14 23:14:24 +0530

    table created

--- procedures.sql
+++ procedures.sql
@@ -15,3 +15,12 @@ CREATE TABLE Courses (
     CourseName VARCHAR(100),
     Credits INT
 );
+
+CREATE TABLE Enrollments (
+    EnrollmentID INT AUTO_INCREMENT PRIMARY KEY,
+    StudentID INT,
+    CourseID INT,
+    EnrollmentDate DATE,
+    FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
+    FOREIGN KEY (CourseID) REFERENCES Courses(CourseID)
+);
